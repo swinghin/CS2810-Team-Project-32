5600aa79ddfedd39318d06a29010b0387f33f528
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2023-01-17 15:11:59 +0000

    Add Employee table class

--- oaxaca_test/restaurantdb/models.py
+++ oaxaca_test/restaurantdb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    employee_id: models.IntegerField()
+    employee_name: models.CharField(max_length=50)
+
+    def __str__(self):
+        return "Employee id:" + self.employee_id + "name" + self.employee_name

bd02d0b56f3d19fef677806bc096a9cfc03ccb88
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2023-01-18 10:29:02 +0000

    Views adds 5 employees and show to webpage

--- oaxaca_test/restaurantdb/views.py
+++ oaxaca_test/restaurantdb/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Employee
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    for i in range(5):
+        Employee.objects.create(
+            employee_name=("Employee "+str(Employee.objects.count()+1)))
+    return HttpResponse(Employee.objects.all())

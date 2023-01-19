3320c91585152c9b639415f18db8bc1cef7b648a
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2023-01-19 09:47:48 +0000

    Add simple dish class

--- oaxaca_test/restaurantdb/models.py
+++ oaxaca_test/restaurantdb/models.py
@@ -7,4 +7,12 @@ class Employee(models.Model):
     employee_name = models.CharField(max_length=50, default=None)
 
     def __str__(self):
-        return "Employee id:" + str(self.id) + "name:" + str(self.employee_name)
+        return self.employee_name
+
+class Dish(models.Model):
+    dish_name = models.CharField(max_length=50, default=None)
+    price = models.DecimalField(max_digits=10,decimal_places=2, default=0.0)
+
+    def __str__(self):
+        return self.dish_name
+

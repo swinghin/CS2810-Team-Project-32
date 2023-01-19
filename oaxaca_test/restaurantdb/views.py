f60c4a3e3eca7f65712dd3e0b185b13c332e0685
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2023-01-19 15:14:02 +0000

    view.py now shows html with employee list

--- oaxaca_test/restaurantdb/views.py
+++ oaxaca_test/restaurantdb/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Employee
 
 # Create your views here.
 
 
 def index(request):
-    for i in range(5):
-        Employee.objects.create(
-            employee_name=("Employee "+str(Employee.objects.count()+1)))
-    return HttpResponse(Employee.objects.all())
+    # for i in range(5):
+    #     Employee.objects.create(
+    #         employee_name=("Employee "+str(Employee.objects.count()+1)))
+    employee_list = Employee.objects.order_by('-id')
+    context = {
+        'employee_list': employee_list
+    }
+    return render(request, 'restaurantdb/index.html', context)

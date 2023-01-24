1ff102a022229ee2c12e99b9fffdcf0eb021a20d
Ismaeel Ali <zjac053@live.rhul.ac.uk>
2023-01-24 23:18:13 +0000

    Added model for dishes in the menu. will try and view them next

--- tutorialsite/menu/models.py
+++ tutorialsite/menu/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Dishes(models.Model):
+    dish_name = models.CharField(max_length=30, default=None)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+
+    def __str__(self):
+        return self.dish_name + ' ' + self.price

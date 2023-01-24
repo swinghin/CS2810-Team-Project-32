75c2441a93e294fdb213ab398a1eb83fb87aaa55
Ismaeel Ali <zjac053@live.rhul.ac.uk>
2023-01-24 23:12:22 +0000

    Added functionlaity for using postgres DB

--- tutorialsite/tutorialsite/settings.py
+++ tutorialsite/tutorialsite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tutorialsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'menu', 
+        'USER': 'user1', 
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 

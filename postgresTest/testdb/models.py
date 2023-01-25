418eb1b625c4d1bd8f33a7278b2e7c09f616cc9b
Khalid <zkac429@live.rhul.ac.uk>
2023-01-25 18:36:04 +0000

    Added some html and edited models

--- postgresTest/testdb/models.py
+++ postgresTest/testdb/models.py
@@ -4,3 +4,8 @@ from django.db import models
 
 class customer(models.Model):
     name = models.CharField(max_length=80)
+
+class Dish(models.Model):
+    Dish_name = models.CharField(max_length=50)
+    Dish_id = models.IntegerField(max_length=3)
+    Dish_price = models.DecimalField(max_digits=5, decimal_places=2)

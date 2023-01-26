963d4b24591da8ffed6a24228fe5c73e44fc1e76
Ismaeel Ali <zjac053@live.rhul.ac.uk>
2023-01-26 14:00:37 +0000

    Created a view which creates menu items and outputs to a test html file

--- tutorialsite/menu/views.py
+++ tutorialsite/menu/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Dishes
 # Create your views here.
+
+def index(request):
+    Dishes.objects.create(
+        dish_name=("Pizza"),
+        price=(1.5)
+    )
+    Dishes.objects.create(
+        dish_name=("Pasta"),
+        price=(1.6)
+    )
+
+    dishes_list = Dish.objects.order_by('-dish_name')
+
+    context = {'dish_list': dish_list}
+
+    return render (request, 'test.html', context)

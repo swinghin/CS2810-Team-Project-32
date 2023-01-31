157a30427076c5cc469fc947c600fdaed89a2167
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2023-01-31 16:13:52 +0000

    Menu now sorted by dish category

--- Oaxaca/restaurant/views.py
+++ Oaxaca/restaurant/views.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,6 +6,7 @@ from django.shortcuts import render
 
 def index(request):
 
+    # Temporary hard-coded dish data for use until proper database is completed
     dish_data = [
         [1, "Apple Pie", 0, 3.50, 1200, False, "DES"],
         [2, "Bagel", 1, 1.50, 300, True, "BKY"],
@@ -16,11 +18,9 @@ def index(request):
         [8, "Blueberry Pie", 0, 3.70, 1150, False, "DES"],
         [9, "Carrot Cake", 0, 4.00, 1000, False, "DES"],
         [10, "Casserole", 0, 400.50, 1200, False, "MAIN"],
-
     ]
 
     dish_list = []
-
     for i in range(len(dish_data)):
         dish_list.append({
             "dish_id": dish_data[i][0],
@@ -32,4 +32,21 @@ def index(request):
             "dish_cat": dish_data[i][6],
         })
 
-    return render(request, 'restaurant/index.html', {"dish_list": dish_list})
+    dish_categories = {
+        "BKY": "Bakery",
+        "BFT": "Breakfast",
+        "MAIN": "Main",
+        "DES": "Dessert",
+    }
+
+    dish_by_categories = defaultdict(list)
+    for dish in dish_list:
+        dish_by_categories[dish["dish_cat"]].append([dish_categories.get(dish["dish_cat"]),dish])
+
+    dish_by_categories.default_factory = None
+
+    # returns a dictionary of dishes sorted by categories and a dictionary of dish categories
+    return render(request, 'restaurant/index.html', {
+        "dish_by_categories": dict(sorted(dish_by_categories.items())),
+        "dish_categories": dict(sorted(dish_categories.items())),
+    })

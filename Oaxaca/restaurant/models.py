4bf82ef0db5c90edcbf844f4e1e57b75e83d2436
Ismaeel Ali <ZJAC053@live.rhul.ac.uk>
2023-02-01 23:33:51 +0000

    Created an allergies table to store types of allergies associated with foods

--- Oaxaca/restaurant/models.py
+++ Oaxaca/restaurant/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Allergies(models.Model):
+    allergies_id = models.AutoField(primary_key = True)
+    allergies_name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.allergies_name
+

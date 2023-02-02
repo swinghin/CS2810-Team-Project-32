b4486b708c4c2984aeb972537e87032c9dfdbdd5
Ismaeel Ali <ZJAC053@live.rhul.ac.uk>
2023-02-02 14:32:10 +0000

    Created a category table so we can group Dishes by category

--- Oaxaca/restaurant/models.py
+++ Oaxaca/restaurant/models.py
@@ -17,3 +17,10 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.ingredient_name
 
+class Category(models.Model):
+    category_id = models.AutoField(primary_key = True)
+    category_name = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.category_name
+

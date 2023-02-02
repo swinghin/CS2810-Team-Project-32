444c52a019c53a29e6644f1765e72fac16811866
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2023-02-02 22:37:38 +0000

    (-) and (+) buttons now modify dish order count

--- Oaxaca/restaurant/static/css/menu.js
+++ Oaxaca/restaurant/static/css/menu.js
@@ -14,6 +14,31 @@ viewMore.forEach(dropdown => {
     });
 });
 
+// Functionality for each dish card (-) and (+) buttons
+const dishOrderSubBtns = document.querySelectorAll('.dish-count-sub');
+const dishOrderAddBtns = document.querySelectorAll('.dish-count-add');
+const MIN_ORDER_COUNT = 1;
+const MAX_ORDER_COUNT = 10;
+dishOrderSubBtns.forEach(subBtn => {
+    let dishCountInput = subBtn.nextElementSibling;
+    if (dishCountInput) {
+        subBtn.addEventListener('click', () => {
+            if (dishCountInput.value > MIN_ORDER_COUNT) {
+                dishCountInput.value = parseInt(dishCountInput.value) - 1;
+            }
+        });
+    }
+});
+dishOrderAddBtns.forEach(addBtn => {
+    let dishCountInput = addBtn.previousElementSibling;
+    if (dishCountInput) {
+        addBtn.addEventListener('click', () => {
+            if (dishCountInput.value < MAX_ORDER_COUNT) {
+                dishCountInput.value = parseInt(dishCountInput.value) + 1;
+            }
+        });
+    }
+});
 
 // Function for filtering menu items in menu
 function filterMenu() {

5884c5dc5f855f60f5cabfca590d9e5b9cb2f15d
zjac053 <ismaeelali12@hotmail.co.uk>
2023-02-02 23:13:55 +0000

    Created Status table which stores the status names an order can have

--- Oaxaca/restaurant/models.py
+++ Oaxaca/restaurant/models.py
@@ -45,3 +45,8 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.table_id
+
+class Status(models.Model):
+    status_id = models.AutoField(primary_key = True)
+    status_name = models.CharField(max_length = 20)
+

549f36d05fd3cf43c0daeac9cc5f2b33fe844caa
Ibrahim Abdi <ZJAC222@live.rhul.ac.uk>
2023-02-03 12:37:56 +0000

    Revert "the initial branch for the login view"
    This reverts commit 2144ebd9fad3a8272ae56052cbebcd12db1faca6.

--- my_django_tutorial/dbCreate/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DbcreateConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dbCreate'
--- my_django_tutorial/dbCreate/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Drinks(models.Model):
-    name = models.CharField(max_length=80)
-    age = models.IntegerField()

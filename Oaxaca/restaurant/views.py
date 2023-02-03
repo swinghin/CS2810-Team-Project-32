673bacb7420cad01edd972ecd4b1e5864a813fc0
Ethan Crowson <zkac173@live.rhul.ac.uk>
2023-02-03 01:53:27 +0000

    Full functionality for viewing order times and marking as delivered added.

--- Oaxaca/restaurant/views.py
+++ Oaxaca/restaurant/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from restaurant.models import Order
-from django.http import HttpResponse
 from django.template.loader import render_to_string
 # Create your views here.
 
 def showorder(request):
-    orders_queryset = Order.objects.filter(order_finish=False)
+    orders_all = Order.objects.all()
+    orders_queryset = orders_all.filter(order_finish=False)
     context = {
         "orders": orders_queryset
     }
-    HTML_STRING = render_to_string("index.html",context=context)
-    return HttpResponse(HTML_STRING)
+    if request.method == "POST":
+        id_list = request.POST.getlist("boxes")
+        for x in id_list:
+            Order.objects.filter(pk=int(x)).update(order_finish=True)
+    return render(request, "index.html", context=context)
+  
 

b2e017d0da8e08e93dcf66d565746252dc773040
Ethan Crowson <zkac173@live.rhul.ac.uk>
2023-02-03 00:48:09 +0000

    Basic CRUD for orders. Checkbox for order finish not yet functional

--- Oaxaca/restaurant/models.py
+++ Oaxaca/restaurant/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Order(models.Model):
+    order_id = models.AutoField(primary_key = True)
+    order_time = models.TimeField()
+    table_id = models.IntegerField()
+    order_finish = models.BooleanField()
+    dish_id = models.IntegerField()
+    status_code = models.CharField(max_length=100)
+    class Meta:
+        db_table = "Order"
+
 class Allergies(models.Model):
     allergies_id = models.AutoField(primary_key = True)
     allergies_name = models.CharField(max_length = 50)
--- Oaxaca/restaurant/views.py
+++ Oaxaca/restaurant/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from restaurant.models import Order
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 # Create your views here.
+
+def showorder(request):
+    orders_queryset = Order.objects.filter(order_finish=False)
+    context = {
+        "orders": orders_queryset
+    }
+    HTML_STRING = render_to_string("index.html",context=context)
+    return HttpResponse(HTML_STRING)
+

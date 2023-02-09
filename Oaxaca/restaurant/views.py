a425774daebf99cb5463bd37daf58c10d91402f4
Ibrahim Abdi <zjac222@live.rhul.ac.uk>
2023-02-09 14:26:41 +0000

    Added redirection to the login and reg pages
    This will allow users to click the links to redirect them to the
    respective pages if they are yet to make an account, or have already
    made one and need to login. Successfully logging in now directs you to
    a homepage that can be edited to any other template or directory.

--- Oaxaca/restaurant/views.py
+++ Oaxaca/restaurant/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import  render, redirect
 from .forms import CreateNewUser
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 def redirect_request(request):
     return render(request=request, template_name="home.html")
+
 def register_request(request):
 	if request.method == "POST":
 		form = CreateNewUser(request.POST)
@@ -16,3 +18,21 @@ def register_request(request):
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = CreateNewUser()
 	return render (request=request, template_name="register.html", context={"register_form":form})
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("restaurant:homepage") # change this value if you want to have the login page redirect somwhere.
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="login.html", context={"login_form":form})

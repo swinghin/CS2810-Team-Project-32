dcf7f5d3563aea0dc7c25bfa738c9be6b38d2807
Wing Hin Shih <wing.shih.2020@live.rhul.ac.uk>
2023-02-23 21:05:58 +0000

    Restore missing index.html

--- Oaxaca/restaurant/urls.py
+++ Oaxaca/restaurant/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "restaurant"   
+app_name = "restaurant"
 
 
 urlpatterns = [
@@ -9,8 +9,8 @@ urlpatterns = [
     path("home/", views.redirect_request, name="homepage"),
     path("login/", views.login_request, name="login"),
     path('pay/', views.payment, name='pay'),
-    path('', views.index, name='index'),
     path('manage/menu/', views.staff_menu, name='staff-menu'),
     path('manage/menu/edit/<int:id>',
          views.staff_dish_details, name='staff-dish-details'),
+    path('', views.index, name='index'),
 ]

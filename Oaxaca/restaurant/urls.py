ae0f1bc10f6e6764350dc8a210383ffae6883d5c
Wing Hin Shih <wing.shih.2020@live.rhul.ac.uk>
2023-02-23 21:09:39 +0000

    Restore footer_public.html

--- Oaxaca/restaurant/urls.py
+++ Oaxaca/restaurant/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = "restaurant"
-
-
 urlpatterns = [
     path("register/", views.register_request, name="register"),
     path("home/", views.redirect_request, name="homepage"),

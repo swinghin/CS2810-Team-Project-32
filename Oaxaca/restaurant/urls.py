1c384fe6cad9eefa9d09363ca399dd9b9285e579
Wing Hin Shih <wing.shih.2020@live.rhul.ac.uk>
2023-02-23 21:32:14 +0000

    Fix url structure in html

--- Oaxaca/restaurant/urls.py
+++ Oaxaca/restaurant/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "restaurant"  
+
+
 urlpatterns = [
     path("register/", views.register_request, name="register"),
     path("home/", views.redirect_request, name="homepage"),

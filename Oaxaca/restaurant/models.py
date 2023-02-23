3d6831ef50bb0095a2c86bf3f71e81051baf9194
Wing Hin Shih <wing.shih.2020@live.rhul.ac.uk>
2023-02-23 21:59:18 +0000

    Rebuild models.py

--- Oaxaca/restaurant/models.py
+++ Oaxaca/restaurant/models.py
@@ -61,58 +61,6 @@ class Status(models.Model):
         InProgress = 'Order in progress'
         Received = 'Received'
         Cooking = 'Cooking'
-        Problem = 'Problem'
-
-    status_id = models.AutoField(primary_key=True)
-    status_name = models.CharField(
-        max_length=20, choices=Statuses.choices, default=Statuses.InProgress)
-
-    def __str__(self):
-        return str(self.status_name)
-
-
-class Order(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    order_time = models.DateTimeField()
-    table_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    order_finish = models.BooleanField(default=False)
-    dish_id = models.ManyToManyField(Dish)
-    status_id = models.ForeignKey(Status, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return "Order no: " + str(self.order_id)
-        return str(self.category_name)
-
-
-class Dish(models.Model):
-    dish_id = models.AutoField(primary_key=True)
-    dish_name = models.CharField(max_length=75)
-    dish_quantity = models.IntegerField()
-    dish_price = models.DecimalField(max_digits=7, decimal_places=2)
-    dish_calories = models.IntegerField()
-    dish_availability = models.BooleanField(default=False)
-    ingredient_id = models.ManyToManyField(Ingredient)
-    category_id = models.ManyToManyField(Category)
-
-    def __str__(self):
-        return str(self.dish_name)
-
-
-class Customer(models.Model):
-    table_id = models.AutoField(primary_key=True)
-    total_price = models.DecimalField(max_digits=11, decimal_places=2)
-    persons = models.IntegerField()
-    need_help = models.BooleanField(default=False)
-
-    def __str__(self):
-        return str(self.table_id)
-
-
-class Status(models.Model):
-    class Statuses(models.TextChoices):
-        InProgress = 'Order in progress'
-        Received = 'Received'
-        Cooking = 'Cooking'
         Ready = 'Ready to serve'
         Delivered = 'Delivered'
         Problem = 'Problem'

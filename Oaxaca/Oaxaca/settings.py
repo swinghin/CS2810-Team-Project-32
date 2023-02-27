2a0333bd4e6f5d1c58500a043e059f45e8d122c0
Ibrahim Abdi <zjac222@live.rhul.ac.uk>
2023-02-27 11:18:02 +0000

    empty commit of notif system

--- Oaxaca/Oaxaca/settings.py
+++ Oaxaca/Oaxaca/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'Oaxaca.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'restaurantDB', 
+        'NAME': 'newDatabase', 
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
